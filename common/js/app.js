40c2c65378889c321f79f5283e2453952593a9cc
Rakesh Pandey <rakesh.pandey10@gmail.com>
2020-05-25 11:26:23 +0530

    changes to main nav and case study section

--- common/js/app.js
+++ common/js/app.js
@@ -178,25 +178,22 @@ $(function () {
 let modalData = [
 	{
 		title: "Design Thinking - develop an idea",
-		description: "We have followed the design thinking process to develop the idea to create a chatbot for VU international students. The process help us to develop an understanding of the international students for whom we were designing the product. This helps us to create empathy towards the international students studying at Victoria University and the advantage of brainstorming session is to define the students needs, problems and gather more insights. The next step in design thinking process is to create ideas for smart and intelligent chatbot and starts the hands on approach in prototyping and testing.",
-		subtitle: "sdfldsjf",
-		desc: "sdfslkj",
-		img: "common/images/design_thinking.jpg",
-		href: "common/images/documents/Chatbot_Proposal_Document.pdf",
-		label: "Download Chatbot Proposal"
+		content: `We have followed the design thinking process to develop the idea to create a chatbot for VU international students. The process help us to develop an understanding of the international students for whom we were designing the product. This helps us to create empathy towards the international students studying at Victoria University and the advantage of brainstorming session is to define the students needs, problems and gather more insights. The next step in design thinking process is to create ideas for smart and intelligent chatbot and starts the hands on approach in prototyping and testing.
+			<p><a href="common/images/documents/Chatbot_Proposal_Document.pdf" title="Download Chatbot Proposal" /></p>
+			<img src="common/images/design_thinking.jpg" alt="Design Thinking - develop an idea" />
+		`
 	},
 	{
 		title: "User Research",
-		description: "During the process, we have started in-depth look into the full UX Process we involved into. In the due course for a month in a group of five, we’ve discovered problems, identified solutions, implemented designs and continuously tested prototype against personas and subjects.",
-		subtitle: "Market Research",
-		desc: "According to research, each year more than 200,000 international students from over 170 countries choose to come to Victoria to study. In 2018, Victoria university had more than 40,000 enrolled students and 12,600 of them are international students who come from more than 100 countries. In addition, the number of international students entering Australia has increased by 800,000 from 1994 to 2019. The rising number of international students not only shows Australian institutions have been successful in recruiting and retaining international students, but also provides them with a big challenge about how to meet a growing need for international students’ guidance and assistance. We started taking a series of interview with our users and gathered information about their use of VU Chatbo as well as how frequent they use the chatbot for queries.",
-		img: "",
-		href: "www.google.com",
-		label: "Download Chatbot Proposal"
+		content: `During the process, we have started in-depth look into the full UX Process we involved into. In the due course for a month in a group of five, we’ve discovered problems, identified solutions, implemented designs and continuously tested prototype against personas and subjects.
+			<h4>Market Research</h4>
+			<p>According to research, each year more than 200,000 international students from over 170 countries choose to come to Victoria to study. In 2018, Victoria university had more than 40,000 enrolled students and 12,600 of them are international students who come from more than 100 countries. In addition, the number of international students entering Australia has increased by 800,000 from 1994 to 2019. The rising number of international students not only shows Australian institutions have been successful in recruiting and retaining international students, but also provides them with a big challenge about how to meet a growing need for international students’ guidance and assistance. We started taking a series of interview with our users and gathered information about their use of VU Chatbo as well as how frequent they use the chatbot for queries.</p>
+			<p><a href="www.google.com" title="Download Chatbot Proposal"></a></p>
+		`
 	},
 	{
 		title: "Usability Testing",
-		description: `Additionally, we started usability testing using an existing chatbot and during regular conversations with the users, we identified most of the users are not comfortable while using the chatbot, and also the experience is equally not too great. Following are the pain points discovered during the process:
+		content: `Additionally, we started usability testing using an existing chatbot and during regular conversations with the users, we identified most of the users are not comfortable while using the chatbot, and also the experience is equally not too great. Following are the pain points discovered during the process:
 			<ul>
 				<li>Getting acquainted with a new country and place</li>
 				<li>Figuring out the transportation system and routes</li>
@@ -209,42 +206,29 @@ let modalData = [
 				<li>Making new connections and network</li>
 				<li>Finding opportunities in a competitive world</li>
 			</ul>
-		`,
-		subtitle: "sdfdsfsg",
-		desc: "sdfds",
-		img: "",
-		href: "www.google.com",
-		label: "Download Chatbot Proposal"
+			<p><a href="www.google.com" title="Download Chatbot Proposal"></a></p>
+		`
 	},
 	{
 		title: "User Persona",
-		description: "Using our research data and interviews we combined our findings to create a persona that would be typical users of an international student.",
-		subtitle: "",
-		desc: "Jaques is a former actor from France who wants to transition his career into Digital Media. He wanted to focus on Digital Marketing for his plans for entrepreneurship. He wants to get acquainted with the university and its services. And for someone who recently moved to Australia, he wants to gain more connections. He is also in need of assistance regarding time and stress management.",
-		img: "",
-		href: "www.google.com",
-		label: "Download Chatbot Proposal"
+		content: `Using our research data and interviews we combined our findings to create a persona that would be typical users of an international student.
+			<p>Jaques is a former actor from France who wants to transition his career into Digital Media. He wanted to focus on Digital Marketing for his plans for entrepreneurship. He wants to get acquainted with the university and its services. And for someone who recently moved to Australia, he wants to gain more connections. He is also in need of assistance regarding time and stress management.</p>
+			<p><a href="www.google.com" title="Download Chatbot Proposal"></a></p>
+		`
 	},
 	{
 		title: "Customer Journey",
-		description: `
+		content: `
 			A customer journey map is a visual representation of the experience that our students have with the chatbot. It helps to tell the story of a customer's experience with your brand from original engagement and into hopefully a long-term relationship.
 			<p>To investigate the need further, interviews are conducted to determine the most common pain points for an international student whose name is Jaques Crow and there are several aspects of pain points for him. The first one is to get acquainted with a new environment and a new culture. He might be confronted with issues of cultural shock. The second is to find the right place for accommodation with an acceptable budget. And then it is not easy to understand the enrollment process and contents for units as well as keep up with schedules and deadlines. The fourth aspect is that he lacks resources to seek support and gain information about all kinds of activities. Making new connections with others and finding opportunities in a competitive world is the last aspect of pain points for him.</p>
-		`,
-		subtitle: "sldskfjsdl",
-		desc: "sdfdsgsd",
-		img: "common/images/design_thinking.jpg",
-		href: "www.google.com",
-		label: "Download Chatbot Proposal"
+		`
 	},
 	{
-		title: "Voice Tone &amp; Style Guide",
-		description: "We have followed the design thinking process to develop the idea to create a chatbot for VU international students. The process help us to develop an understanding of the international students for whom we were designing the product. This helps us to create empathy towards the international students studying at Victoria University and the advantage of brainstorming session is to define the students needs, problems and gather more insights. The next step in design thinking process is to create ideas for smart and intelligent chatbot and starts the hands on approach in prototyping and testing.",
-		subtitle: "sdfksdlssdgdsf",
-		desc: "sdfdsge",
-		img: "common/images/design_thinking.jpg",
-		href: "www.google.com",
-		label: "Download Chatbot Proposal"
+		title: "Customer Journey",
+		content: `
+			A customer journey map is a visual representation of the experience that our students have with the chatbot. It helps to tell the story of a customer's experience with your brand from original engagement and into hopefully a long-term relationship.
+			<p>To investigate the need further, interviews are conducted to determine the most common pain points for an international student whose name is Jaques Crow and there are several aspects of pain points for him. The first one is to get acquainted with a new environment and a new culture. He might be confronted with issues of cultural shock. The second is to find the right place for accommodation with an acceptable budget. And then it is not easy to understand the enrollment process and contents for units as well as keep up with schedules and deadlines. The fourth aspect is that he lacks resources to seek support and gain information about all kinds of activities. Making new connections with others and finding opportunities in a competitive world is the last aspect of pain points for him.</p>
+		`
 	}
 ];
 
@@ -270,11 +254,7 @@ const initializeModal = () => {
 				if (index === i)
 					return `			
 					<h2>${data.title}</h2>
-					<p>${data.description}</p>
-					<p><a href="${data.href}" title="test" target="_blank">${data.label}</a></p>
-					<p><img src='${data.img}' width="100%" alt="${data.title}" /></p>
-					<h4>${data.subtitle}</h4>
-					<p>${data.desc}</p>`
+					<p>${data.content}</p>
 			}).join("")}
 			</div>	
 			`;
